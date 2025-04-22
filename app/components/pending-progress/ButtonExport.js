c301bfa05d73495044c1ca058c10e7405dcaefab
baichuni zain <baichuni.zain@bankbsi.co.id>
2025-04-22 14:44:00 +0700

    solving error download excel grafik pending progress

--- app/components/pending-progress/ButtonExport.js
+++ app/components/pending-progress/ButtonExport.js
@@ -1,15 +1,10 @@
-import React, { useState,useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import Button from '../ui/Button';
 import {
     fetchExcelDetail,
     fetchExcelProgress,
     fetchExcelPending
 } from './getData';
-
-import {
-    formatMainData,
-    formatSummaryData
-} from './formatExcel';
 
 import {
     createExportExcel,
@@ -21,95 +16,180 @@ export default function ButtonExport({ startDate, endDate, region, area }) {
     const [dataProgress, setDataProgress] = useState([]);
     const [dataPending, setDataPending] = useState([]);
 
+    const params = { startDate, endDate, region, area };
+
+    const getDetail = async () => {
+        const res = await fetchExcelDetail(params);
+        setDataDetail(res || []);
+    }
+
+    const geProgress = async () => {
+        const res = await fetchExcelProgress(params);
+        setDataProgress(res || []);
+    }
+
+    const getPending = async () => {
+        const res = await fetchExcelPending(params);
+        setDataPending(res || []);
+    }
+
     const handleButtonClick = () => {
-        const params = { startDate, endDate, region, area };
 
-        const detail = fetchExcelDetail(params);
-        const progress = fetchExcelProgress(params);
-        const pending = fetchExcelPending(params);
+        getDetail()
+        geProgress()
+        getPending()
 
-        setDataDetail(detail || []);
-        setDataProgress(progress || []);
-        setDataPending(pending || []);
-
-        if (detail.length === 0) {
+        if (dataDetail.length === 0) {
             alert("Data Excel export Detail Wise tidak ada / kosong !");
             return;
         }
-        if (progress.length === 0) {
+        if (dataProgress.length === 0) {
             alert("Data Excel export Inprogress Wise tidak ada / kosong !");
             return;
         }
-        if (pending.length === 0) {
+        if (dataPending.length === 0) {
             alert("Data Excel export Pending Wise tidak ada / kosong !");
             return;
         }
 
-        const formattedDataDetail = detail.map(formatMainData)
+        const formattedDataDetail = dataDetail.map((item) => {
+            const newItem = {
+                "TGL INPUT": item.TGL_INPUT || "",
+                "NO APLIKASI": item.NO_APLIKASI || "",
+                "NAMA NASABAH": item.NAMA_NASABAH || "",
+                "JENIS PRODUK": item.JENIS_PRODUK || "",
+                "KODE PROGRAM": item.EVENT || "",
+                "PLAFOND": item.PLAFOND || "",
+                "CABANG": item.NAMA_CABANG || "",
+                "AREA": item.NAMA_AREA || "",
+                "REGION": item.REGION || "",
+                "LAST POSISI": item.LAST_POSISI || "",
+                "LAST READ BY": item.LAST_READ_BY + " - " + item.LAST_READ_BY_NAME || "",
+                "LAST UPDATE": item.LAST_UPDATE || "",
+                "JUMLAH RETURN": item.JUM_RETURN || 0,
+                "BRANCH DDE": item.BRANCH_DDE || "",
+            };
 
-        const formattedDataProgress = progress.map(formatSummaryData)
+            return newItem;
+        });
+
+        const formattedDataProgress = dataProgress.map((item, index) => {
+            const newItem = {
+                "NO": index + 1,
+                "REGION": item.REGION || "",
+                "NAMA AREA": `${item.NAMA_AREA}` || "",
+                "TOTAL APLIKASI": item.TOTAL_APLIKASI || 0,
+                "IDE": item.IDE || 0,
+                "DEDUPE": item.DEDUPE || 0,
+                "IDEB": item.IDEB || 0,
+                "UPLOAD DOC": item.UPLOAD_DOC || 0,
+                "DDE": item.DDE || 0,
+                "VERIN": item.VERIN || 0,
+                "OTOR VERIN": item.OTOR_VERIN || 0,
+                "VALID": item.VALID || 0,
+                "APPROVAL": item.APPROVAL || 0,
+                "SP3": item.SP3 || 0,
+                "AKAD": item.AKAD || 0,
+                "OTOR AKAD": item.OTOR_AKAD || 0,
+                "REVIEW": item.REVIEW || 0,
+                "OTOR REVIEW": item.OTOR_REVIEW || 0,
+                "LIVE": item.LIVE || 0,
+                "CANCEL": item.CANCEL || 0,
+                "REJECT": item.REJECT || 0
+            };
+
+            return newItem;
+        });
         formattedDataProgress.push({
             "NO": "Total",
             "REGION": "",
             "NAMA AREA": "",
-            "TOTAL APLIKASI": progress[0].SUM_TOTAL || 0,
-            "IDE": progress[0].SUM_IDE || 0,
-            "DEDUPE": progress[0].SUM_DEDUPE || 0,
-            "IDEB": progress[0].SUM_DEDUPE || 0,
-            "UPLOAD DOC": progress[0].SUM_UPLOAD || 0,
-            "DDE": progress[0].SUM_DDE || 0,
-            "VERIN": progress[0].SUM_VERIN || 0,
-            "OTOR VERIN": progress[0].SUM_OTOR_VERIN || 0,
-            "VALID": progress[0].SUM_VALID || 0,
-            "APPROVAL": progress[0].SUM_APPROVAL || 0,
-            "SP3": progress[0].SUM_SP3 || 0,
-            "AKAD": progress[0].SUM_AKAD || 0,
-            "OTOR AKAD": progress[0].SUM_OTOR_AKAD || 0,
-            "REVIEW": progress[0].SUM_REVIEW || 0,
-            "OTOR REVIEW": progress[0].SUM_OTOR_REVIEW || 0,
-            "LIVE": progress[0].SUM_LIVE || 0,
-            "CANCEL": progress[0].SUM_CANCEL || 0,
-            "REJECT": progress[0].SUM_REJECT || 0
+            "TOTAL APLIKASI": dataProgress[0].SUM_TOTAL || 0,
+            "IDE": dataProgress[0].SUM_IDE || 0,
+            "DEDUPE": dataProgress[0].SUM_DEDUPE || 0,
+            "IDEB": dataProgress[0].SUM_DEDUPE || 0,
+            "UPLOAD DOC": dataProgress[0].SUM_UPLOAD || 0,
+            "DDE": dataProgress[0].SUM_DDE || 0,
+            "VERIN": dataProgress[0].SUM_VERIN || 0,
+            "OTOR VERIN": dataProgress[0].SUM_OTOR_VERIN || 0,
+            "VALID": dataProgress[0].SUM_VALID || 0,
+            "APPROVAL": dataProgress[0].SUM_APPROVAL || 0,
+            "SP3": dataProgress[0].SUM_SP3 || 0,
+            "AKAD": dataProgress[0].SUM_AKAD || 0,
+            "OTOR AKAD": dataProgress[0].SUM_OTOR_AKAD || 0,
+            "REVIEW": dataProgress[0].SUM_REVIEW || 0,
+            "OTOR REVIEW": dataProgress[0].SUM_OTOR_REVIEW || 0,
+            "LIVE": dataProgress[0].SUM_LIVE || 0,
+            "CANCEL": dataProgress[0].SUM_CANCEL || 0,
+            "REJECT": dataProgress[0].SUM_REJECT || 0
         })
 
-        const formattedDataPending = pending.map(formatSummaryData)
+        const formattedDataPending = dataPending.map((item, index) => {
+            const newItem = {
+                "NO": index + 1,
+                "REGION": item.REGION || "",
+                "NAMA AREA": `${item.NAMA_AREA}` || "",
+                "TOTAL APLIKASI": item.TOTAL_APLIKASI || 0,
+                "IDE": item.IDE || 0,
+                "DEDUPE": item.DEDUPE || 0,
+                "IDEB": item.IDEB || 0,
+                "UPLOAD DOC": item.UPLOAD_DOC || 0,
+                "DDE": item.DDE || 0,
+                "VERIN": item.VERIN || 0,
+                "OTOR VERIN": item.OTOR_VERIN || 0,
+                "VALID": item.VALID || 0,
+                "APPROVAL": item.APPROVAL || 0,
+                "SP3": item.SP3 || 0,
+                "AKAD": item.AKAD || 0,
+                "OTOR AKAD": item.OTOR_AKAD || 0,
+                "REVIEW": item.REVIEW || 0,
+                "OTOR REVIEW": item.OTOR_REVIEW || 0,
+                "LIVE": item.LIVE || 0,
+                "CANCEL": item.CANCEL || 0,
+                "REJECT": item.REJECT || 0
+            };
+
+            return newItem;
+        });
         formattedDataPending.push({
             "NO": "Total",
             "REGION": "",
             "NAMA AREA": "",
-            "TOTAL APLIKASI": pending[0].SUM_TOTAL || 0,
-            "IDE": pending[0].SUM_IDE || 0,
-            "DEDUPE": pending[0].SUM_DEDUPE || 0,
-            "IDEB": pending[0].SUM_DEDUPE || 0,
-            "UPLOAD DOC": pending[0].SUM_UPLOAD || 0,
-            "DDE": pending[0].SUM_DDE || 0,
-            "VERIN": pending[0].SUM_VERIN || 0,
-            "OTOR VERIN": pending[0].SUM_OTOR_VERIN || 0,
-            "VALID": pending[0].SUM_VALID || 0,
-            "APPROVAL": pending[0].SUM_APPROVAL || 0,
-            "SP3": pending[0].SUM_SP3 || 0,
-            "AKAD": pending[0].SUM_AKAD || 0,
-            "OTOR AKAD": pending[0].SUM_OTOR_AKAD || 0,
-            "REVIEW": pending[0].SUM_REVIEW || 0,
-            "OTOR REVIEW": pending[0].SUM_OTOR_REVIEW || 0,
-            "LIVE": pending[0].SUM_LIVE || 0,
-            "CANCEL": pending[0].SUM_CANCEL || 0,
-            "REJECT": pending[0].SUM_REJECT || 0
+            "TOTAL APLIKASI": dataPending[0].SUM_TOTAL || 0,
+            "IDE": dataPending[0].SUM_IDE || 0,
+            "DEDUPE": dataPending[0].SUM_DEDUPE || 0,
+            "IDEB": dataPending[0].SUM_DEDUPE || 0,
+            "UPLOAD DOC": dataPending[0].SUM_UPLOAD || 0,
+            "DDE": dataPending[0].SUM_DDE || 0,
+            "VERIN": dataPending[0].SUM_VERIN || 0,
+            "OTOR VERIN": dataPending[0].SUM_OTOR_VERIN || 0,
+            "VALID": dataPending[0].SUM_VALID || 0,
+            "APPROVAL": dataPending[0].SUM_APPROVAL || 0,
+            "SP3": dataPending[0].SUM_SP3 || 0,
+            "AKAD": dataPending[0].SUM_AKAD || 0,
+            "OTOR AKAD": dataPending[0].SUM_OTOR_AKAD || 0,
+            "REVIEW": dataPending[0].SUM_REVIEW || 0,
+            "OTOR REVIEW": dataPending[0].SUM_OTOR_REVIEW || 0,
+            "LIVE": dataPending[0].SUM_LIVE || 0,
+            "CANCEL": dataPending[0].SUM_CANCEL || 0,
+            "REJECT": dataPending[0].SUM_REJECT || 0
         })
 
         const headers = ["TGL INPUT", "NO APLIKASI", "NAMA NASABAH", "JENIS PRODUK", "KODE PROGRAM",
             "PLAFOND", "CABANG", "AREA", "REGION", "LAST POSISI", "LAST READ BY", "LAST UPDATE",
             "JUMLAH RETURN", "BRANCH DDE"
         ];
-        const headers1 = ["No","Region","Nama Area","Total Aplikasi","IDE","Dedupe","iDEB","Upload Doc",
-            "DDE","Verin","Otor Verin","Valid","Approval","SP3","Akad","Otor Akad","Review",]
+        const headers1 = ["No", "Region", "Nama Area", "Total Aplikasi", "IDE", "Dedupe", "iDEB", "Upload Doc",
+            "DDE", "Verin", "Otor Verin", "Valid", "Approval", "SP3", "Akad", "Otor Akad", "Review",
+            "Otor Review", "Live", "Cancel", "Reject"
+        ]
 
         const today = new Date();
         const options = { day: '2-digit', month: '2-digit', year: 'numeric' };
         const formattedDate = today.toLocaleDateString('id-ID', options);
 
         createExportExcel(
-            formattedDataDetail, 
+            formattedDataDetail,
             headers,
             'Detail Data',
             `${startDate}_${endDate}_PENDING_PROGRESS_DETAIL_WISE.xlsx`,
