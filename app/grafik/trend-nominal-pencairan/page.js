6ddab31a428eb58b1f6d54ecbe25fa933c0d5dae
baichuni zain <baichuni.zain@bankbsi.co.id>
2025-05-09 09:54:00 +0700

    ketinggalan

---
+++ app/grafik/trend-nominal-pencairan/page.js
@@ -0,0 +1,16 @@
+"use client";
+
+import React, { useEffect, useRef, useState } from "react";
+
+const trenpencairan = () => {
+    return (
+        <div className="p-6 bg-gray-100 min-h-screen">
+            <div className="bg-white p-6 rounded-lg shadow-md">
+            <h2 className="text-lg text-center font-semibold mt-4">Comming Soon</h2>
+            <h3 className="text-lg text-center font-semibold mb-4">Trend Nominal Pencairan</h3>
+            </div>
+        </div>
+    );
+};
+
+export default trenpencairan;
